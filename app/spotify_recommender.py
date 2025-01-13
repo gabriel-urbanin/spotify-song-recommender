b6327383d39e2564a1553b17f283318df5edffc7
gabriel-urbanin <gabrielurbanin@gmail.com>
2025-01-12 21:28:38 -0300

    Added default recommendation

--- app/spotify_recommender.py
+++ app/spotify_recommender.py
@@ -7,14 +7,14 @@ from mlxtend.frequent_patterns import fpgrowth, association_rules
 VOLUME__PATH = '/app/model/song_recommendations.pkl'
 DATASET_URL = 'https://raw.githubusercontent.com/gabriel-urbanin/song-recommender-config/refs/heads/main/spotify_dataset.csv'
 
-def fetch_and_process_dataset():
+def fetch_and_process_dataset() -> None:
     df_playlists = _fetch_dataset()
     rules = _create_rules_from_dataset(df_playlists)
     recommendations = _create_song_recommendations(rules)
 
     _export_song_recommendations(recommendations)
 
-def _fetch_dataset():
+def _fetch_dataset() -> None:
     try:
         df = pd.read_csv(DATASET_URL)
         print('Dataset successfully fetched from Github and loaded as Dataframe!')
@@ -47,11 +47,24 @@ def _create_song_recommendations(rules: pd.DataFrame) -> defaultdict:
     
     recommendations = defaultdict(set)
     for antecedents, consequents in zip(rules['antecedents'], rules['consequents']):
-        recommendations[antecedents].update(consequents)
+        for antecedent in antecedents:
+            recommendations[antecedent].update(consequents)    
+
+    recommendations['default_recommendation'].update(_get_songs_with_high_support(rules))
 
     return recommendations
 
-def _export_song_recommendations(recommendations):
+def _get_songs_with_high_support(rules: pd.DataFrame) -> list:
+    rules_sorted_by_support = rules.sort_values(by='support', ascending=False)
+    top_5_most_popular_songs = rules_sorted_by_support['antecedents'].head(5).tolist()
+    
+    default_recommendations = []
+    for song in top_5_most_popular_songs:
+        default_recommendations.extend(song)
+        
+    return default_recommendations
+
+def _export_song_recommendations(recommendations: defaultdict) -> None:
     try:
         with open(VOLUME__PATH, 'rb') as file:
             print('A previous set of recommendations was found. Updating it with new recommendations...')
